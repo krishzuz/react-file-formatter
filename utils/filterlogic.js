7da75fd906cd86ae083e8315edb2bf20302a5386
Kishore <krishkl2001@gmail.com>
2024-11-27 20:47:08 +0530

    fix: formatting

--- utils/filterlogic.js
+++ utils/filterlogic.js
@@ -1,46 +1,45 @@
-const order = require("./custom.json").order;
-const getOrderIndex = (node) => {
-  if (node.type === "ReturnStatement") {
-    return order.indexOf("return");
+const getOrderIndex = (node, order) => {
+  if (node.type === 'ReturnStatement') {
+    return order.indexOf('return');
   }
 
   if (
-    node.type === "ExpressionStatement" &&
-    node.expression.type === "CallExpression"
+    node.type === 'ExpressionStatement' &&
+    node.expression.type === 'CallExpression'
   ) {
     // Direct hook calls like `useEffect`, `useContext`
     const callee = node.expression.callee.name;
     return order.indexOf(callee) !== -1 ? order.indexOf(callee) : -1;
   }
 
-  if (node.type === "VariableDeclaration") {
+  if (node.type === 'VariableDeclaration') {
     const declarator = node.declarations[0];
-    if (declarator.init?.type === "CallExpression") {
+    if (declarator.init?.type === 'CallExpression') {
       // Hook assignments like `const [state, setState] = useState()`
       const callee = declarator.init.callee.name;
       return order.indexOf(callee) !== -1 ? order.indexOf(callee) : -1;
     }
 
-    if (declarator.init?.type === "ArrowFunctionExpression") {
+    if (declarator.init?.type === 'ArrowFunctionExpression') {
       // Arrow functions
-      return order.indexOf("ArrowFunctionExpression");
+      return order.indexOf('ArrowFunctionExpression');
     }
 
-    if (declarator.init?.type === "ObjectPattern") {
+    if (declarator.init?.type === 'ObjectPattern') {
       // Destructuring hooks, like `const { mutate } = useMutation()`
       const properties = declarator.init.properties || [];
       if (
-        properties.some((prop) => prop.key.name === "mutate") &&
+        properties.some((prop) => prop.key.name === 'mutate') &&
         declarator.init.arguments?.length
       ) {
-        return order.indexOf("useMutation");
+        return order.indexOf('useMutation');
       }
     }
   }
 
-  if (node.type === "FunctionDeclaration") {
+  if (node.type === 'FunctionDeclaration') {
     // Named function declarations
-    return order.indexOf("function");
+    return order.indexOf('function');
   }
 
   return -1; // Default for unhandled nodes
