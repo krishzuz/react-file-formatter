d85f220fc1f12476f9805be96fc57fef106ad5b5
Kishore <krishkl2001@gmail.com>
2024-11-27 20:47:44 +0530

    fix: formatting & code refactor

--- src/index.js
+++ src/index.js
@@ -1,34 +1,50 @@
 #!/usr/bin/env node
-const generate = require("@babel/generator").default;
-const { existsSync, readFileSync, writeFileSync } = require("fs");
-const parser = require("@babel/parser").parse;
-const traverse = require("@babel/traverse").default;
-const { getOrderIndex } = require("../utils/filterlogic");
-const { type } = require("os");
-const { isArray } = require("util");
+const generate = require('@babel/generator').default;
+const { existsSync, readFileSync, writeFileSync, write } = require('fs');
+const parser = require('@babel/parser').parse;
+const traverse = require('@babel/traverse').default;
+const { getOrderIndex } = require('../utils/filterlogic');
+const { formatAndAddSpaces } = require('../utils/prettierConfig');
+const configJSONDefault = require('../utils/custom.json');
 
-const [, , filePath] = process.argv;
+const [, , filePath, configPath = ''] = process.argv;
+
+let configJSON = configJSONDefault;
+
+// check the config file exist or not - only run if config file is provided or use the default one
+if (configPath.length > 0) {
+  if (!existsSync(configPath)) {
+    console.error('Config file not found:', configPath);
+    process.exit(1);
+  } else {
+    configJSON = JSON.parse(readFileSync(configPath, 'utf-8'));
+  }
+}
 
 // check the file exist or not
 if (!existsSync(filePath)) {
-  console.error("File not found:", filePath);
+  console.error('File not found:', filePath);
   process.exit(1);
 } else {
   // read the file
-  const readFile = readFileSync(filePath, "utf-8");
+  const readFile = readFileSync(filePath, 'utf-8');
 
   async function callParser() {
     // parsing the file
     const ast = await fileparser(readFile);
-
     const output = generate(ast);
 
     if (output.code) {
-      // outputting the file at the same location
-      writeFileSync(filePath, output.code);
-      console.log("File formatted:", filePath);
+      // formatting the file
+      const prettierOutput = await formatAndAddSpaces(
+        output.code,
+        configJSON.prettier
+      );
+      // after fomatting a file save it
+      writeFileSync(filePath, prettierOutput);
+      console.log('File formatted:', filePath);
     } else {
-      console.log("Something went wrong - Please Report the issuse");
+      console.log('Something went wrong - Please Report the issuse');
     }
   }
   callParser();
@@ -42,20 +58,28 @@ if (!existsSync(filePath)) {
  */
 async function fileparser(readFile) {
   const result = parser(readFile, {
-    sourceType: "module",
-    plugins: ["jsx", "typescript"],
+    sourceType: 'module',
+    plugins: ['jsx', 'typescript'],
   });
 
   traverse(result, {
     FunctionDeclaration(path) {
       const body = path.node.body.body;
-      body.sort((a, b) => getOrderIndex(a) - getOrderIndex(b));
+      body.sort(
+        (a, b) =>
+          getOrderIndex(a, configJSON.order) -
+          getOrderIndex(b, configJSON.order)
+      );
     },
     ArrowFunctionExpression(path) {
       const { body } = path.node;
       // Check if the body is a block statement
-      if (body.type === "BlockStatement" && Array.isArray(body.body)) {
-        body.body.sort((a, b) => getOrderIndex(a) - getOrderIndex(b));
+      if (body.type === 'BlockStatement' && Array.isArray(body.body)) {
+        body.body.sort(
+          (a, b) =>
+            getOrderIndex(a, configJSON.order) -
+            getOrderIndex(b, configJSON.order)
+        );
       }
     },
   });
